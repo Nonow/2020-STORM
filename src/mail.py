9e35a12ec218af9de1656750574cd928ad9be0da
Sayanthan1 <60175817+Sayanthan1@users.noreply.github.com>
2020-03-18 12:05:15 +0100

    a test

---
+++ src/mail.py
@@ -0,0 +1,88 @@
+include "ESP32_MailClient.h"
+
+
+const char* ssid = "";	//a adapter
+const char* password = ""; //a adapter
+
+//  port 465 (SSL) 
+// YOU MUST ENABLE less secure app option https://myaccount.google.com/lesssecureapps?pli=1
+define emailSenderAccount    "sayan.sath@gmail.com"    
+define emailSenderPassword   "modifier"
+define emailRecipient        "sayan.sath@gmail.com"
+define smtpServer            "smtp.gmail.com"
+define smtpServerPort        465
+define emailSubject          "ESP32 Test"
+
+// The Email Sending data object contains config and data to send
+SMTPData smtpData;
+
+// Callback function to get the Email sending status
+void sendCallback(SendStatus info);
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  
+  Serial.print("Connecting");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected.");
+  Serial.println();
+  Serial.println("Preparing to send email");
+  Serial.println();
+  
+  // Set the SMTP Server Email host, port, account and password
+  smtpData.setLogin(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword);
+
+  // For library version 1.2.0 and later which STARTTLS protocol was supported,the STARTTLS will be 
+  // enabled automatically when port 587 was used, or enable it manually using setSTARTTLS function.
+  //smtpData.setSTARTTLS(true);
+
+  // Set the sender name and Email
+  smtpData.setSender("ESP32", emailSenderAccount);
+
+  // Set Email priority or importance High, Normal, Low or 1 to 5 (1 is highest)
+  smtpData.setPriority("High");
+
+  // Set the subject
+  smtpData.setSubject(emailSubject);
+
+  // Set the message with HTML format
+  smtpData.setMessage("<div style=\"color:#2f4468;\"><h1>Hello World!</h1><p>- Sent from ESP32 board</p></div>", true);
+  // Set the email message in text format (raw)
+  //smtpData.setMessage("Hello World! - Sent from ESP32 board", false);
+
+  // Add recipients, you can add more than one recipient
+  smtpData.addRecipient(emailRecipient);
+  smtpData.addRecipient("sayan.sathia@gmail.com");
+
+  smtpData.setSendCallback(sendCallback);
+
+  //Start sending Email, can be set callback function to track the status
+  if (!MailClient.sendMail(smtpData))
+    Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
+
+  //Clear all data from Email object to free memory
+  smtpData.empty();
+}
+
+void loop() {
+  
+}
+
+// Callback function to get the Email sending status
+void sendCallback(SendStatus msg) {
+  // Print the current status
+  Serial.println(msg.info());
+
+  // Do something when complete
+  if (msg.success()) {
+    Serial.println("----------------");
+  }
+}
